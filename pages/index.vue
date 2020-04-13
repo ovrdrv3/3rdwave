5fa15fc7d4a53f78aa386abfc965889937a508ac
Donaven Snowden <donaven.snowden@gmail.com>
2020-04-13 14:59:01 -0700

    style homepage, add window dimensions to calculate css

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,76 @@
 <template>
   <div>
-    <b-nav>
+    <b-nav :vertical="narrowDevice">
       <b-nav-item active>3rdwave</b-nav-item>
-      <b-nav-item>pricing</b-nav-item>
       <b-nav-item>what we do</b-nav-item>
+      <b-nav-item>pricing</b-nav-item>
+      <b-nav-item>contact us</b-nav-item>
     </b-nav>
-    <div id="pond">
-      <h1 class="heading">
+    <div
+      id="centerCallToAction"
+      class="wheat-text"
+      :style="{
+        top: windowHeight / 2 - 50 + 'px',
+        left: windowWidth / 2 - 130 + 'px'
+      }"
+      style="position: absolute;"
+    >
+      <h2>
         your orders.
         <br />
         <span class="italic">
           ready for pick up.
         </span>
-      </h1>
+      </h2>
+      <b-button variant="primary" class="sienna-text wheat-bg"
+        >Contact Us</b-button
+      >
     </div>
   </div>
+  <!-- </div> -->
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+  data() {
+    return {
+      windowHeight: null,
+      windowWidth: null
+    }
+  },
+  computed: {
+    narrowDevice() {
+      return this.windowWidth <= 576
+    }
+  },
+  mounted() {
+    this.windowHeight = window.innerHeight
+    this.windowWidth = window.innerWidth
+  }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;1,700&display=swap');
+.btn-primary:hover,
+.btn-primary:focus,
+.btn-primary:active,
+.btn-primary.active,
+.open > .dropdown-toggle.btn-primary {
+  color: wheat !important;
+  background-color: sienna !important;
+  border-color: sienna !important;
+}
+
+.btn-primary {
+  border-color: wheat !important;
+}
 
 html {
   height: 100%;
 }
+
 body {
   height: 100%;
   background: url('/coffee_mug-matt-hoffman-unsplash.jpg') no-repeat center
@@ -41,16 +83,16 @@ body {
   font-family: 'Playfair Display', serif;
 }
 
-#pond {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 94vh;
+.sienna-text {
+  color: sienna;
 }
 
-.heading {
-  position: relative;
+.wheat-text {
   color: wheat;
+}
+
+.wheat-bg {
+  background-color: wheat;
 }
 
 .italic {
@@ -58,6 +100,10 @@ body {
 }
 
 .nav-item > a {
+  font-size: 1.25em;
   color: wheat !important;
+}
+.nav-item > a:hover {
+  color: sienna !important;
 }
 </style>
